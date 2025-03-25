70c4ffab4c356aae203a6020a65e78834e62dcdd
Koredeee <muhammad.fawwaz003@binus.ac.id>
2025-03-25 22:57:39 +0700

    Initial Commit

---
+++ Looca/LoocaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoocaApp.swift
+//  Looca
+//
+//  Created by FWZ on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LoocaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
