f9d2b5bf70583aed6df29f06eeddde254a3d85e8
ppasyani <ppasyani@gmail.com>
2025-04-07 09:17:06 +0700

    feat(direction): add direction view feature

---
+++ Looca/NavCard.swift
@@ -0,0 +1,18 @@
+//
+//  NavCard.swift
+//  Looca
+//
+//  Created by Patricia Putri Art Syani on 07/04/25.
+//
+
+import SwiftUI
+
+struct NavCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavCard()
+}
---
+++ Looca/NavView.swift
@@ -0,0 +1,18 @@
+//
+//  NavView.swift
+//  Looca
+//
+//  Created by Patricia Putri Art Syani on 07/04/25.
+//
+
+import SwiftUI
+
+struct NavView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavView()
+}
